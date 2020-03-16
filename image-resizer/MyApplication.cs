61cfbaf6121f4ddaec165ea18cfb91f97d93afce
Seann Alexander <self@seannalexander.com>
2020-03-16 01:54:04 -0400

    Add Logging Support

---
+++ image-resizer/MyApplication.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using ImageMagick;
+using Microsoft.Extensions.Logging;
+
+namespace image_resizer
+{
+    public class MyApplication
+    {
+        private readonly ILogger _logger;
+        public static char file_separator = System.IO.Path.DirectorySeparatorChar;
+        public MyApplication(ILogger<MyApplication> logger)
+        {
+            _logger = logger;
+        }
+        internal void Run()
+        {
+
+            _logger.LogInformation("Resizing Image(s)");
+
+            string in_file_base = $@".{file_separator}Test{file_separator}ImageIn";
+            string out_file_base = $@".{file_separator}Test{file_separator}ImageOut";
+
+            _logger.LogInformation("Starting Resize of Batch:");
+
+            System.Diagnostics.Stopwatch stopWatch = System.Diagnostics.Stopwatch.StartNew();
+            resizeImagesInPath(in_file_base, out_file_base);
+            _logger.LogInformation("Elapsed time\t\t{0} ms", stopWatch.ElapsedMilliseconds);
+
+            _logger.LogInformation("Completed.");
+        }
+        internal void resizeImagesInPath(string in_file_base, string out_file_base)
+        {
+            int file_width = 100;
+            int file_height = 75;
+
+            VerifyAndCreateDirectories(in_file_base, out_file_base);
+            string[] FilesFromFolder = GetFilesFromFolder(in_file_base);
+
+            //System.Threading.Tasks.Parallel.ForEach(FilesFromFolder, (in_file_name) =>
+            foreach (string in_file_name in FilesFromFolder)
+            {
+                string file_extension = System.IO.Path.GetExtension(in_file_name);
+                string file_name_without_extension = System.IO.Path.GetFileNameWithoutExtension(in_file_name);
+                string out_file_path = $@"{out_file_base}{file_separator}{file_name_without_extension}.{file_width}x{file_height}{file_extension}";
+
+                bool OperationStatus = resizeImageFromPath_Width_x_Height(in_file_name, out_file_path, file_width, file_height);
+            }
+            //});
+        }
+
+        internal void VerifyAndCreateDirectories(string in_file_base, string out_file_base)
+        {
+
+            if (!System.IO.Directory.Exists(out_file_base))
+            {
+                System.IO.Directory.CreateDirectory(out_file_base);
+            }
+        }
+
+        internal string[] GetFilesFromFolder(string in_file_base)
+        {
+            string[] FilesFromFolder = new string[] { };
+
+            if (System.IO.Directory.Exists(in_file_base))
+            {
+                FilesFromFolder = System.IO.Directory.GetFiles(in_file_base);
+            }
+            return FilesFromFolder;
+        }
+
+        internal bool resizeImageFromPath_Width_x_Height(string in_file_name, string out_file_path, int file_width, int file_height)
+        {
+            bool OperationStatus = false;
+            // Read from file
+            using (MagickImageCollection collection = new MagickImageCollection(in_file_name))
+            {
+                // Read from file
+                collection.Coalesce();
+                foreach (MagickImage image in collection)
+                {
+                    System.Diagnostics.Stopwatch stopWatch = System.Diagnostics.Stopwatch.StartNew();
+                    MagickGeometry size = new MagickGeometry(file_width, file_height);
+                    size.IgnoreAspectRatio = true;
+
+                    image.Resize(size);
+
+                    // Save the result
+                    try
+                    {
+                        image.Write(out_file_path);
+                        OperationStatus = true;
+                    }
+                    catch (Exception _Exception)
+                    {
+                        _logger.LogError("Exception: {0}", _Exception.ToString());
+                        OperationStatus = false;
+                    }
+                    string json_out =
+$@"{{
+    file_path: ""{out_file_path}"",
+    elaspsed_time: ""{stopWatch.ElapsedMilliseconds}"",
+    file_width: ""{file_width}"",
+    file_height: ""{file_height}""
+}},";
+                    _logger.LogInformation(json_out);
+                }
+            }
+            return OperationStatus;
+        }
+    }
+}
--- image-resizer/Program.cs
+++ image-resizer/Program.cs
@@ -1,99 +1,39 @@
-﻿using ImageMagick;
-using System;
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace image_resizer
 {
     class Program
     {
-        public static char file_separator = System.IO.Path.DirectorySeparatorChar;
         static void Main(string[] args)
         {
-            Console.WriteLine("Resizing Image(s)");
+            var services = new ServiceCollection();
+            ConfigureServices(services);
 
-            string in_file_base = $@".{file_separator}Test{file_separator}ImageIn";
-            string out_file_base = $@".{file_separator}Test{file_separator}ImageOut";
-            
-            Console.WriteLine("Starting Resize of Batch:");
-            
-            System.Diagnostics.Stopwatch stopWatch = System.Diagnostics.Stopwatch.StartNew();
-            resizeImagesInPath(in_file_base, out_file_base);
-            Console.WriteLine("Elapsed time\t\t{0} ms", stopWatch.ElapsedMilliseconds);
-            
-            Console.WriteLine("Completed.");
-            Console.ReadLine();
-        }
-
-        static void resizeImagesInPath(string in_file_base, string out_file_base)
-        {
-            int file_width = 100;
-            int file_height = 75;
-
-            VerifyAndCreateDirectories(in_file_base, out_file_base);
-            string[] FilesFromFolder = GetFilesFromFolder(in_file_base);
-
-            System.Threading.Tasks.Parallel.ForEach(FilesFromFolder, (in_file_name) =>
+            using (ServiceProvider serviceProvider = services.BuildServiceProvider())
             {
-                string file_extension = System.IO.Path.GetExtension(in_file_name);
-                string file_name_without_extension = System.IO.Path.GetFileNameWithoutExtension(in_file_name);
-                string out_file_path = $@"{out_file_base}{file_separator}{file_name_without_extension}.{file_width}x{file_height}{file_extension}";
+                MyApplication app = serviceProvider.GetService<MyApplication>();
+                app.Run();
+            }
+        }
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddTransient<MyApplication>()
+                    //.AddScoped<IBusinessLayer, CBusinessLayer>()
+                    //.AddSingleton<IDataAccessLayer, CDataAccessLayer>();
+                    ;
 
-                bool OperationStatus = resizeImageFromPath_Width_x_Height(in_file_name, out_file_path, file_width, file_height);
+            var serilogLogger = new LoggerConfiguration()
+            .WriteTo.File("Log.txt")
+            .CreateLogger();
+
+            services.AddLogging(builder =>
+            {
+                builder.SetMinimumLevel(LogLevel.Trace);
+                builder.AddSerilog(logger: serilogLogger, dispose: true);
             });
-        }
-
-        private static void VerifyAndCreateDirectories(string in_file_base, string out_file_base)
-        {
-
-            if (!System.IO.Directory.Exists(out_file_base))
-            {
-                System.IO.Directory.CreateDirectory(out_file_base);
-            }
-        }
-
-        private static string[] GetFilesFromFolder(string in_file_base)
-        {
-            string[] FilesFromFolder = new string[] { };
-
-            if (System.IO.Directory.Exists(in_file_base))
-            {
-                FilesFromFolder = System.IO.Directory.GetFiles(in_file_base);
-            }
-            return FilesFromFolder;
-        }
-
-        static bool resizeImageFromPath_Width_x_Height(string in_file_name, string out_file_path, int file_width, int file_height)
-        {
-            bool OperationStatus = false;
-            // Read from file
-            using (MagickImageCollection collection = new MagickImageCollection(in_file_name))
-            {
-                // Read from file
-                collection.Coalesce();
-                foreach (MagickImage image in collection)
-                {
-                    //Console.WriteLine("\tStarting Resize:");
-                    System.Diagnostics.Stopwatch stopWatch = System.Diagnostics.Stopwatch.StartNew();
-                    MagickGeometry size = new MagickGeometry(file_width, file_height);
-                    // This will resize the image to a fixed size without maintaining the aspect ratio.
-                    // Normally an image will be resized to fit inside the specified size.
-                    size.IgnoreAspectRatio = true;
-
-                    image.Resize(size);
-
-                    // Save the result
-                    image.Write(out_file_path);
-                    string json_out = 
-$@"{{
-    file_path: ""{out_file_path}"",
-    elaspsed_time: ""{stopWatch.ElapsedMilliseconds}"",
-    file_width: ""{file_width}"",
-    file_height: ""{file_height}""
-}},";
-                    Console.WriteLine(json_out);
-                    OperationStatus = true;
-                }
-            }
-            return OperationStatus;
         }
     }
 }
